ba74468ee133f6024eb0a72edaee0cf9e4852eb5
longshao5520 <longshao5520@outlook.com>
2023-02-23 17:20:37 +0800

    update: Component library

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,32 @@
 <script setup lang="ts">
-import { UploadFilled } from "@element-plus/icons-vue";
-import { reactive, ref } from "vue";
-import { get } from "lodash";
-
-import { useFileListStore } from "./store/useFileListStore";
-
-import { useDark, useToggle } from "@vueuse/core";
+import { ref } from "vue";
+import { useDark } from "@vueuse/core";
+const loadCss = (url: string, isCache = false) => {
+  const element = document.createElement("link");
+  element.setAttribute("rel", "stylesheet");
+  element.setAttribute("type", "text/css");
+  if (isCache) {
+    element.setAttribute("href", url + "?t=" + new Date().getTime());
+  } else {
+    element.setAttribute("href", url);
+  }
+  document.head.appendChild(element);
+};
 const isDark = ref(false);
 useDark({
   onChanged: (IsDark) => {
     isDark.value = IsDark;
+    if (IsDark) {
+      loadCss("https://unpkg.com/ant-design-vue/dist/antd.dark.min.css");
+    } else {
+      loadCss("https://unpkg.com/ant-design-vue/dist/antd.min.css");
+    }
   },
 });
-
-// console.log(isDark);
-
-// const toggleDark = useToggle(isDark);
-
-const store = useFileListStore();
-
-let form = reactive({
-  api: "https://file.cs08.cn/file/uploadUtil",
-  postName: "file",
-  jsonUrl: "data.url",
-});
-
-const handleSuccess = (response: any, file: any, fileList: any) => {
-  console.log(response, file, fileList);
-
-  const url = get(response, form.jsonUrl);
-  const name = file.name;
-  const type = file.raw.type;
-  store.addFile({ url, name, type });
-};
 </script>
 
 <template>
-  <!-- <a-config-provider :prefix-cls="isDark ? 'custom-dark' : 'ant'"> -->
-    <router-view></router-view>
-  <!-- </a-config-provider> -->
-  <!-- <el-main> -->
-  <!-- <el-form :model="form" label-width="100px">
-      <el-form-item label="API地址">
-        <el-input v-model="form.api" placeholder="API地址" />
-      </el-form-item>
-      <el-form-item label="POST参数名">
-        <el-input v-model="form.postName" placeholder="POST参数名" />
-      </el-form-item>
-      <el-form-item label="JSON路径">
-        <el-input v-model="form.jsonUrl" placeholder="eg: data.url" />
-      </el-form-item>
-    </el-form>
-    <el-upload
-      class="upload-demo"
-      drag
-      multiple
-      :limit="1"
-      :action="form.api"
-      :show-file-list="false"
-      :on-success="handleSuccess"
-    >
-      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-    </el-upload> -->
-
-  <!-- <el-table :data="store.files" style="width: 100%">
-      <el-table-column prop="name" label="文件名" />
-      <el-table-column prop="type" label="文件类型" />
-      <el-table-column prop="url" label="文件地址">
-      </el-table-column>
-      <el-table-column prop="url" label="预览">
-        <template #default="{ row }">
-          <el-image
-            :src="row.url"
-            :preview-src-list="[row.url]"
-            fit="scale-down"
-            style="width: 100px"
-          ></el-image>
-        </template>
-      </el-table-column>
-    </el-table> -->
-  <!-- </el-main> -->
+  <router-view></router-view>
 </template>
 
-<style scoped>
-/* .logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-} */
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-
-import Antd from "ant-design-vue";
-// import 'ant-design-vue/dist/antd.css';
-import 'ant-design-vue/dist/antd.dark.css'
-
-import { createPinia } from "pinia";
-import { createPersistedState } from "pinia-plugin-persistedstate";
-
-const store = createPinia();
-// 状态持久化-插件配置
-store.use(
-  createPersistedState({
-    storage: window.localStorage,
-    serializer: {
-      // 指定参数序列化器
-      serialize: JSON.stringify,
-      deserialize: JSON.parse,
-    },
-  })
-);
-
 import router from "./router";
+import Antd from "ant-design-vue";
 
-createApp(App)
-  .use(router)
-  .use(store)
-  .use(Antd)
-  .mount("#app");
+createApp(App).use(router).use(Antd).mount("#app");
