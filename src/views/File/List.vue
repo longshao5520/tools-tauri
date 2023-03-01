74669624a2b7ddba4201e4b4d180a3a494d582ef
longshao5520 <longshao5520@outlook.com>
2023-03-01 17:17:00 +0800

    update: import card layout

--- src/views/File/List.vue
+++ src/views/File/List.vue
@@ -3,6 +3,7 @@ import { createFromIconfontCN } from "@ant-design/icons-vue";
 import { reactive } from "vue";
 import { useFileUploadHistory } from "../../api/file";
 import { useNotification } from "../../hooks/useNotification";
+import CardLayout from "@/components/CardLayout.vue";
 
 const { select } = useFileUploadHistory();
 
@@ -13,7 +14,6 @@ const store = reactive({
 const fetch = async () => {
   const { data } = await select(100, 1);
   store.list = data;
-  "111".includes("1");
 };
 
 fetch();
@@ -30,58 +30,29 @@ const findIcon = (type: string) => {
 };
 
 const { sendSystemNotification } = useNotification();
-const copyFile = async (url: string) => {
-  console.log(url);
+const copyFile = async (item: any) => {
+  console.log(item.url);
 
   await sendSystemNotification(`文件地址已复制到剪切板`);
 };
 </script>
 
 <template>
-  <!-- {{ store.list }} -->
   <a-empty v-if="store.list.length == 0" description="暂无数据" />
-  <a-row v-else :gutter="20" style="min-width: 648px">
-    <a-col
-      v-for="(item, index) in store.list"
-      :key="index"
-      :xs="12"
-      :sm="8"
-      :md="6"
-      :lg="4"
-      style="margin-bottom: 20px; user-select: none"
-      @click="copyFile(item.url)"
-    >
-      <a-card>
-        <div style="display: flex; flex-direction: column; align-items: center">
-          <div style="width: 100%; height: 150px" class="flex-center">
-            <a-image
-              v-if="item.type.includes('image')"
-              :src="item.url"
-              :preview="false"
-              width="100%"
-              height="150px"
-            />
-            <icon-font
-              v-else
-              :type="findIcon(item.type)"
-              style="font-size: 5rem"
-            />
-          </div>
-          <div
-            style="
-              width: 100%;
-              margin-top: 15px;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              white-space: nowrap;
-            "
-          >
-            {{ item.name }}
-          </div>
-        </div>
-      </a-card>
-    </a-col>
-  </a-row>
+  <CardLayout v-else :list="store.list" title-key="name" @item-click="copyFile">
+    <template #icon="{ item }">
+      <div style="width: 100%; height: 150px" class="flex-center">
+        <a-image
+          v-if="item.type.includes('image')"
+          :src="item.url"
+          :preview="false"
+          width="100%"
+          height="150px"
+        />
+        <icon-font v-else :type="findIcon(item.type)" style="font-size: 5rem" />
+      </div>
+    </template>
+  </CardLayout>
 </template>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,28 @@
 <script setup lang="ts">
-// import { UploadFilled } from "@element-plus/icons-vue";
-import { CloudUploadOutlined } from "@ant-design/icons-vue";
 import { useRouter } from "vue-router";
+import CardLayout from "@/components/CardLayout.vue";
 
 const router = useRouter();
 
-const skip = () => {
+const list = [
+  {
+    title: "文件上传",
+    icon: "CloudUploadOutlined",
+    iconStyle: {
+      fontSize: "120px",
+      color: "#F0F",
+    },
+  },
+];
+
+const skip = (item: any) => {
   router.push("/file/upload");
 };
 </script>
 
 <template>
   <a-layout-content style="padding: 20px">
-    <a-row :gutter="20" style="min-width: 648px">
-      <a-col :xs="12" :sm="8" :md="6" :lg="4">
-        <a-card @click="skip">
-          <div
-            style="display: flex; flex-direction: column; align-items: center"
-          >
-            <a-space>
-              <cloud-upload-outlined style="font-size: 120px; color: #F0F;" />
-            </a-space>
-            <div style="margin-top: 10px">文件上传</div>
-          </div>
-        </a-card>
-      </a-col>
-    </a-row>
+    <CardLayout :list="list" @item-click="skip"></CardLayout>
   </a-layout-content>
 </template>
 
