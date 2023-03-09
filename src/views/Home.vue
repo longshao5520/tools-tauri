9d4441f8a4024852ae3de0c79a672e22585a2e25
longshao5520 <longshao5520@outlook.com>
2023-03-09 19:49:26 +0800

    update: home modules list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useRouter } from "vue-router";
+import {useRouter} from "vue-router";
 import CardLayout from "@/components/CardLayout.vue";
 
 const router = useRouter();
@@ -7,16 +7,26 @@ const router = useRouter();
 const list = [
   {
     title: "文件上传",
+    path: "/file/upload",
     icon: "CloudUploadOutlined",
     iconStyle: {
       fontSize: "120px",
       color: "#F0F",
     },
   },
+  {
+    title: "设置",
+    path: "/setting",
+    icon: "SettingOutlined",
+    iconStyle: {
+      fontSize: "120px",
+      color: "#F00",
+    },
+  }
 ];
 
 const skip = (item: any) => {
-  router.push("/file/upload");
+  router.push(item.path);
 };
 </script>
 
